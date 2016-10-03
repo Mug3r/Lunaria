475652fbeea562b8cd7b07153a2fc6b903b55e06
Mug3r <moug3r@gmail.com>
2016-10-03 19:45:13 +0200

    Update Game.java
    Comments

--- Lunaria/Lunaria/src/Main/Game.java
+++ Lunaria/Lunaria/src/Main/Game.java
@@ -3,7 +3,7 @@ package Main;
 import javax.swing.JFrame;
 
 public class Game {
-
+	//Main method runs on launch creating a new gamepanel object and frame in which it can be found
 	public static void main(String[]args){
 		
 		JFrame window = new JFrame("Lunaria");
