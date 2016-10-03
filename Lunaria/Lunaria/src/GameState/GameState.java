ac4ebf6906d2b3de33d63ed82f08a5029bb94908
Mug3r <moug3r@gmail.com>
2016-10-03 19:29:00 +0200

    Update GameState.java
    Comments

--- Lunaria/Lunaria/src/GameState/GameState.java
+++ Lunaria/Lunaria/src/GameState/GameState.java
@@ -3,7 +3,7 @@ package GameState;
 public abstract class GameState {
 
 	protected GameStateManager gsm;
-	
+	//Abstract methods for the controllign of gamestates and their interactions with keypresses.
 	public abstract void init();
 	public abstract void update();
 	public abstract void draw(java.awt.Graphics2D g);
