a3a768f70d3b402235d2e1d650900c9c8ab28e6d
Mug3r <moug3r@gmail.com>
2016-10-03 19:51:37 +0200

    Update Tile.java
    Comments

--- Lunaria/Lunaria/src/TileMap/Tile.java
+++ Lunaria/Lunaria/src/TileMap/Tile.java
@@ -10,13 +10,14 @@ public class Tile {
 	//tile types
 	public static final int NORMAL = 0;
 	public static final int BLOCKED = 1;
-	
+	//Constructor
 	public Tile(BufferedImage image, int type){		
 		this.image = image;
 		this.type  = type;		
 	}
-	
+	//returns the tile's image
 	public BufferedImage getImage(){return image;}
+	//returns the type of tile blocked(cannot be moved through) or normal(can be passed through)
 	public int getType(){return type;}
 	
 }
