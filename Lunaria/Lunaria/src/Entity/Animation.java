e39d3a476dba46321deff2d2b05918df0f25928f
Mug3r <moug3r@gmail.com>
2016-10-03 18:56:28 +0200

    Update Animation.java

--- Lunaria/Lunaria/src/Entity/Animation.java
+++ Lunaria/Lunaria/src/Entity/Animation.java
@@ -11,21 +11,22 @@ public class Animation {
 	private long delay;
 	
 	private boolean playedOnce;
-	
+	//Prepares the new animation by reseting the hasplayed once variable 
 	public Animation() {
 		playedOnce = false;
 	}
-	
+	//Sets which frame the animation will begin with after recieving an array of images containing frames
 	public void setFrames(BufferedImage[] frames) {
 		this.frames = frames;
 		currentFrame = 0;
 		startTime = System.nanoTime();
 		playedOnce = false;
 	}
-	
+	//Sets the delay between frames
 	public void setDelay(long d) { delay = d; }
+	//Sets the current frame
 	public void setFrame(int i) { currentFrame = i; }
-	
+	//Runs the animation based on how much time has passed vs  the delay required.
 	public void update() {
 		
 		if(delay == -1) return;
@@ -41,9 +42,11 @@ public class Animation {
 		}
 		
 	}
-	
+	//Returns the index of the current frame
 	public int getFrame() { return currentFrame; }
+	//returns the specific image(frame)
 	public BufferedImage getImage() { return frames[currentFrame]; }
+	//Returns the hasplayedonce variable
 	public boolean hasPlayedOnce() { return playedOnce; }
 	
 }
